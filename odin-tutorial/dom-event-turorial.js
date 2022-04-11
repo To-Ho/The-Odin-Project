a2336c2ad3be78da3c5bc36eb214c59bb6f788d2
To-Ho <quocanho1992@gmail.com>
2022-04-11 15:10:46 -0600

    Review dom-event tutorial

--- odin-tutorial/dom-event-turorial.js
+++ odin-tutorial/dom-event-turorial.js
@@ -1,49 +1,38 @@
-/*
-//Method 2
-const btn1 = document.querySelector('#btn1');
-btn1.onclick = () => alert('Hello World 2'); //arrow function
+//Method 2: onclick js part
+const btn = document.querySelector('#btn');
+btn.onclick = () => alert("Hello World");
 
-//Method 3
+//Method 3: addEventListener, js part
 const btn2 = document.querySelector('#btn2');
 btn2.addEventListener('click', () => {
-    alert('Hello World 3!');
+    alert("Hello Method 3");
 });
 
-//Function
+//Named function
 function alertFunction() {
-    alert('Yay, function!');
+    alert("Named function YAY!");
 }
+//Method 2
+btn.onclick = alertFunction;
+//Method 3
+btn2.addEventListener('click', alertFunction);
 
-//method 2
-const btnFunc2 = document.querySelector('.btnFunc2');
-btnFunc2.onclick = alertFunction;
-
-//method 3
-const btnFunc3 = document.querySelector('.btnFunc3');
-btnFunc3.addEventListener('click', alertFunction);
-
-//addEventListener()
-const btn = document.querySelector('#btn');
-btn.addEventListener('Click', function(e) { //Note that function (e) is a callback from addEventListener
-    console.log(e); //Output: [object MouseEvent] {"isTrusted": true}
-});
-//The e in that function is an object that references the event itself. Within that object you have access to many useful properties and functions 
-//such as which mouse button or key was pressed, or information about the event’s target
-
-btn.addEventListener('Click', function(e) {
-    console.log(e.target); //Output: <button id="btnFunc5">Click Me 5</button>
+//Trying out stuffs
+const btn3 = document.querySelector('#btn3');
+btn3.addEventListener('click', function (e) {
+    console.log(e);
+    console.log(e.target);
+    e.target.style.background = 'blue';
 });
 
-btn.addEventListener('click', function (e) {
-    e.target.style.background = 'blue'; //Change click me btn box to blue when click
-});
-*/
-//addEventListener() to group of nodes
-//button is a node list, it looks and act like an Array
-const buttons = document.querySelectorAll('button');
+//Attaching listeners to groups of nodes
+//Use querySelectorAll(''):
+// buttons is a node list. It looks and acts much like an array
+const buttonS = document.querySelectorAll('button');
 
-//Use the forEach()
-buttons.forEach((button) => {
+//Use .forEach method to iterate through each button
+buttonS.forEach((button) => {
+    //and for each one, add a 'click' listener
     button.addEventListener('click', () => {
         alert(button.id);
     });
